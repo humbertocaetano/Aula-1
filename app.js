fa426ad2ec8b80fed6ba50476908760de31240c8
Humberto Caetano <humberto.ccs@gmail.com>
2024-03-27 13:53:34 +0000

    Funcionalidade nova linha

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
     res.send('Hello Geeks!');
+    res.send('\n!');
+    res.send('Nova versão!');
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
